1dcf692a00dfb7bc6c9f12a3f4654f8212ba893d
Grzegorz Laszczyk <grzegorz.laszczyk@xebia.com>
2023-11-16 20:40:45 +0100

    feature: NU-001 Upgrade Next to latest version. Switch from pages to app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import {ReactNode} from "react";
+
+import { Metadata } from 'next'
+import '@/styles/globals.css'
+
+export const metadata: Metadata = {
+	title: 'Home',
+	description: 'Welcome to Next.js',
+}
+
+export default function RootLayout({
+ // Layouts must accept a children prop.
+ // This will be populated with nested layouts or pages
+ children,
+}: {
+	children: ReactNode
+}) {
+	return (
+		<html lang="en">
+		<body>{children}</body>
+		</html>
+	)
+}
