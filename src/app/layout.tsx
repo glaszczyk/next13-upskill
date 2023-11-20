844d2f565c167eef0c43b810a8e28248034729f7
Grzegorz Laszczyk <grzegorz.laszczyk@xebia.com>
2023-11-20 16:11:25 +0100

    feature: NU-002 Add Header to App

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { type ReactNode } from "react";
 
 import { type Metadata } from "next";
 import PrelineLoader from "./prelineLoader";
+import { AppHeader } from "@src/components/index";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -20,7 +21,14 @@ export default function RootLayout({
 		<html lang="en">
 			<body>
 				<PrelineLoader />
-				{children}
+				<div className="relative mx-auto flex  h-screen max-w-[85rem] flex-col p-0">
+					<AppHeader />
+					<main>
+						<div className="container mx-auto h-full pt-20 ">
+							<div className="container mx-auto h-full">{children}</div>
+						</div>
+					</main>
+				</div>
 			</body>
 		</html>
 	);
