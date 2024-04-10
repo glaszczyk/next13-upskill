a0edcab513719601d4d5c8b5136b6ddcb0614446
Grzegorz Laszczyk <grzegorz.laszczyk@xebia.com>
2024-04-10 11:01:21 +0200

    feature: NU-004 Convert products and produc pages to getServerSideProps SSR version

--- pages/products/[productId]/index.tsx
+++ pages/products/[productId]/index.tsx
@@ -1,43 +1,60 @@
-import type { GetStaticProps, InferGetStaticPropsType } from "next";
+import type {
+	GetServerSideProps,
+	GetServerSidePropsContext,
+	InferGetServerSidePropsType,
+} from "next";
+import { useRouter } from "next/router";
 import type { ReactElement } from "react";
+import useSWR, { type Fetcher } from "swr";
 import Layout from "../../../components/Layout";
-import { type IProduct, type IProductResponse } from "@models/products";
+import { type IProduct } from "@models/products";
 import { ProductItem } from "@/components/ProductItem";
 
-const SingleProductPage = ({ productData }: InferGetStaticPropsType<typeof getStaticProps>) => {
-	return <ProductItem product={productData} />;
+const fetcher: Fetcher<Partial<IProduct & { message?: string }>> = (url: string) =>
+	fetch(url).then((r) => r.json());
+
+const SingleProductPage = ({
+	productData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+	const {
+		query: { productId },
+	} = useRouter();
+	const selectedProduct = productId && typeof productId === "string" ? productId : null;
+	const { data, isLoading, error } = useSWR(`/api/products/${selectedProduct}`, fetcher, {
+		fallbackData: { ...productData },
+	});
+	if (isLoading) return <p>Loading...</p>;
+	if (data) {
+		if (data.message) return <p>{data.message}</p>;
+		return <ProductItem product={data as IProduct} />;
+	}
+	return <p>{error}</p>;
 };
 
 SingleProductPage.getLayout = function getLayout(page: ReactElement) {
 	return <Layout>{page}</Layout>;
 };
 
-export const getStaticProps = (async ({ params }) => {
-	const productId = params && typeof params.productId === "string" ? params.productId : "";
-	// Call an external API endpoint to get faq
-	const res = await fetch(`${process.env["APP_URL"]}/api/products/${productId}`);
-	const productData = await res.json();
-
-	// By returning { props: { blogpostListData } }, the BlogPostList component
-	// will receive `blogpostListData` as a prop at build time
+export const getServerSideProps = (async (context: GetServerSidePropsContext) => {
+	const productId = context.query.productId;
+	if (productId && typeof productId === "string") {
+		try {
+			const response = await fetch(`${process.env["APP_URL"]}/api/products/${productId}`);
+			const result: Partial<IProduct & { message?: string }> = await response.json();
+			return {
+				props: {
+					productData: { ...result },
+				},
+			};
+		} catch (errorMessage) {
+			return {
+				props: { productData: { message: "Something went wrong" } },
+			};
+		}
+	}
 	return {
-		props: {
-			productData,
-		},
+		props: { productData: { message: "Something went wrong" } },
 	};
-}) satisfies GetStaticProps<{
-	productData: IProduct;
-}>;
-
-export const getStaticPaths = async () => {
-	// Call an external API endpoint to get faq
-	const res = await fetch(`${process.env["APP_URL"]}/api/products?page=1`);
-	const productsData: IProductResponse = await res.json();
-
-	const paths = productsData.products.map(({ productId }) => ({
-		params: { productId: productId.toString() },
-	}));
-	return { paths, fallback: "blocking" };
-};
+}) satisfies GetServerSideProps<{ productData: Partial<IProduct & { message?: string }> }>;
 
 export default SingleProductPage;
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,3 +1,8 @@
+import {
+	type GetServerSideProps,
+	type GetServerSidePropsContext,
+	type InferGetServerSidePropsType,
+} from "next";
 import { useRouter } from "next/router";
 import type { ReactElement } from "react";
 import useSWR, { type Fetcher } from "swr";
@@ -8,14 +13,19 @@ import { ProductsListItem } from "@/components/ProductsListItem";
 
 const fetcher: Fetcher<IProductResponse> = (url: string) => fetch(url).then((r) => r.json());
 
-const ProductsListPage = () => {
+const ProductsListPage = ({
+	products,
+	meta,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
 	const {
 		query: { page },
 	} = useRouter();
 
 	const activePage = page && typeof page === "string" ? page : 1;
 
-	const { data, isLoading, error } = useSWR(`/api/products?page=${activePage}`, fetcher);
+	const { data, isLoading, error } = useSWR(`/api/products?page=${activePage}`, fetcher, {
+		fallbackData: { products, meta },
+	});
 
 	if (isLoading) return <p>Loading...</p>;
 	if (error) return <p>Error...</p>;
@@ -33,6 +43,16 @@ const ProductsListPage = () => {
 			</>
 		);
 };
+
+export const getServerSideProps = (async (context: GetServerSidePropsContext) => {
+	const activePage =
+		context.query.page && typeof context.query.page === "string" ? context.query.page : "1";
+	const response = await fetch(`${process.env["APP_URL"]}/api/products?page=${activePage}`);
+	const { meta, products } = await response.json();
+	return {
+		props: { products, meta },
+	};
+}) satisfies GetServerSideProps<IProductResponse>;
 
 ProductsListPage.getLayout = function getLayout(page: ReactElement) {
 	return <Layout>{page}</Layout>;
